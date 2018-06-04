bbe8cbf1167ee77fe28788e6bd19e416d05111f9
Christoph Ersfeld <Christoph.Ersfeld@senacor.com>
2018-06-04 19:32:29 +0200

    Container class added

--- src/constants/pathnames.js
+++ src/constants/pathnames.js
@@ -1,7 +1,6 @@
 export const HOME = 'home';
 export const PLAYGROUND = 'playgorund';
 export const HIGHSCORE = 'highscore';
-
 export const PLAYGROUND_INTRO = 'highscore/intro';
 export const PLAYGROUND_GAME = 'highscore/game';
 export const PLAYGROUND_ADD_HIGHSCORE = 'highscore/add_highscore';
